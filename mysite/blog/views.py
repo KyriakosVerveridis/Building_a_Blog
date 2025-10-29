e775bf765fee77802480d9a99b306ad737fbe022
KyriakosVerveridis <ververidiskyriakos@gmail.com>
2025-10-29 21:42:01 +0200

    Adding URLs & Views

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def starting_page(request):
+  """Render the starting (home) page of the blog."""
+  pass
+
+def posts(request):
+  """Display a list of all blog posts."""
+  pass
+
+def post_detail(request):
+  """Show the details for a single blog post."""
+  pass
