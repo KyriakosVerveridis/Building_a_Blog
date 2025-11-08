1435c83d40d8faf50f01be71d220ae4d1e196fc6
KyriakosVerveridis <ververidiskyriakos@gmail.com>
2025-11-08 17:03:30 +0200

    Adding a Post Model

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.core.validators import MinLengthValidator
 
 # Create your models here.
+
+class Post(models.Model):
+	"""
+	Post model represents a blog post.
+	"""
+	title = models.CharField(max_length=150) # Title of the post
+	excerpt = models.CharField(max_length=200) # Short summary or excerpt (optional)
+	image_name = models.CharField(max_length=100) # Name of the image associated with the post
+	date = models.DateField(auto_now=True) # Last modification date, automatically updated
+	slug = models.SlugField(unique=True, db_index=True) # URL-friendly unique identifier
+	content = models.TextField(validators=[MinLengthValidator(10)]) # Full post content
+    
+	
+    
+
+	
+
