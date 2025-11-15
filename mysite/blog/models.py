0213ae7d3d4d6ac942551f60c9aa0a61f3d3bf3c
KyriakosVerveridis <ververidiskyriakos@gmail.com>
2025-11-15 18:57:58 +0200

    Adding an Imagefield to the Post Model

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -35,7 +35,7 @@ class Post(models.Model):
 	"""
 	title = models.CharField(max_length=150) # Title of the post
 	excerpt = models.CharField(max_length=200) # Short summary or excerpt (optional)
-	image_name = models.CharField(max_length=100) # Name of the image associated with the post
+	image = models.ImageField(upload_to="posts", null=True) # Stores an uploaded image for each post.
 	date = models.DateField(auto_now=True) # Last modification date, automatically updated
 	slug = models.SlugField(unique=True, db_index=True) # URL-friendly unique identifier
 	content = models.TextField(validators=[MinLengthValidator(10)]) # Full post content
